420f2bfe5be0803a195b71b0758b0fea0fc88eac
cindychang0607 <cindychang0607@gmail.com>
2017-07-20 14:11:29 -0400

    Update chua_diode.m
    changed vmax, vmin, and resistor values to measured values from circuit and altered some equations to make the modeled curve look similar to circuit iv curve

--- chua_model/chua_diode.m
+++ chua_model/chua_diode.m
@@ -1,8 +1,8 @@
 %%% Chua Diode./Op Amp Circuit IV Curve %%%
 
 % op amp rails
-vmax = 9;
-vmin = -9;
+vmax = 10.66;
+vmin = -10.16;
 
 %input voltage
 step = (vmax-vmin)/10000; %step size
@@ -10,16 +10,16 @@ vin = vmin:step:vmax;     % Vin
 
 %%% Chua diode parameters %%%
 %circuit values   theoretical values
-R1 = 3300;           %3300  ohms
-R2 = 20000;          %22000 ohms
-R3 = 20000;          %22000 ohms
-R4 = 2200;           %2200  ohms
-R5 = 220;            %220   ohms
-R6 = 220;            %221   ohms
+R1 = 3240;           %3300  ohms
+R2 = 21800;          %22000 ohms
+R3 = 21740;          %22000 ohms
+R4 = 2160;           %2200  ohms
+R5 = 219.7;            %220   ohms
+R6 = 221;            %221   ohms
 
 % Esaturation
-Pos_Esat = vmax;
-Neg_Esat = -vmin;
+Pos_Esat = vmax * .57;
+Neg_Esat = -vmin * .72;
 
 % E1 and E2 define limit of piecewise functions
 Pos_E1 = R4/(R5+R4)*Pos_Esat;
@@ -32,7 +32,7 @@ Neg_E2 = R1/(R2+R1)*Neg_Esat;
 % piecewise function slopes
 Ga = (-1/R1 - 1/R4);
 Gb = (1/R3 - 1/R4);
-Gc = (1/R6 + 1/R3);
+Gc = .5* (1/R6 + 1/R3);
 
 % piecewise intercepts
 Pos_b = Pos_E2*(Ga-Gb);
@@ -53,6 +53,8 @@ I4 = (1./(1+exp(mult.*(-vin+Pos_E1)))) .* (vin.*Gc - Pos_c);
 % total current in Chua diode
 I = (I0 + I1 + I2 + I3 + I4);
 
+iv = open('circuit-and-model-iv.fig');
+figure(iv)
+hold on
 plot(vin, I*10);
 grid on;
-
